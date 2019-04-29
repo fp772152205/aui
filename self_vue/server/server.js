400e1953a1afcde4dee955075139af017b6ddf2a
fp772152205 <50104734+fp772152205@users.noreply.github.com>
2019-04-29 20:29:22 +0800

    发第四个月

---
+++ self_vue/server/server.js
@@ -0,0 +1,14 @@
+//允许中文
+SET NAMES UTF-8
+//如果 存在就 丢弃
+DROP DATABASE IF EXERSIT self_vue;
+//创建数据库
+CREATE DATABASES self_vue UTF-8;
+USE self_vue;
+//创建把表格
+CREATE TABLE self_table(
+	uid INT PRIMARY
+
+)
+
+//
